5dcd8ae4c181a4fda81ec2497ac86473a0bcd903
fudongri <393556397@qq.com>
2017-09-02 23:37:30 +0800

    简单重构

--- js/game.js
+++ js/game.js
@@ -1,5 +1,15 @@
 var canvas, ctx, players, total = 200;
 
+window.onload = function () {
+    initEnv(); //初始化环境
+    initMoney(); //初始化筹码
+    var poker = GameProxy.initPoker(); //生成牌
+    poker = GameProxy.shufflePoker(); //洗牌
+    players = GameProxy.createPlayers(); //生成2个玩家
+    players = GameProxy.dealPoker(poker, players); //发牌
+    play();
+}
+
 function drawPlayerZmInit() {
     ctx.drawPokerBack(800, 10, 150, '#7A7BB8', '#2E319C');
     ctx.drawPokerBack(600, 10, 150, '#7A7BB8', '#2E319C');
@@ -13,7 +23,6 @@ function drawPlayerZmInit() {
     ctx.fillStyle = '#519fff';
     ctx.strokeStyle = '#ff626c';
     ctx.fillRect(10, 210, 150, 30);
-    ctx.font = "20px 楷体";
     ctx.fillStyle = '#EEEEEE';
     ctx.fillText("筹 码：" + localStorage.getItem("zm"), 18, 234);
 }
@@ -30,7 +39,6 @@ function drawPlayerMeInit() {
     ctx.fillStyle = '#519fff';
     ctx.strokeStyle = '#ff626c';
     ctx.fillRect(10, 528, 150, 30);
-    ctx.font = "20px 楷体";
     ctx.fillStyle = '#EEEEEE';
     ctx.fillText("筹 码：" + localStorage.getItem("me"), 18, 553);
 }
@@ -39,7 +47,6 @@ function drawMybtn() {
     ctx.fillStyle = '#b064ff';
     ctx.strokeStyle = '#ff626c';
     ctx.fillRect(180, 558, 100, 30);
-    ctx.font = "20px 楷体";
     ctx.fillStyle = '#EEEEEE';
     ctx.fillText("看 牌", 200, 580);
 
@@ -54,38 +61,23 @@ function drawMybtn() {
     ctx.fillText("加 注", 200, 664);
 }
 
-window.onload = function () {
-    canvas = document.getElementById("canvas");
-    ctx = canvas.getContext('2d');
-    ctx.font = "20px 楷体";
-    initMoney();
-    //测试
-    players = createPlayers();
-
-    if (localStorage.getItem("me") < 100) {
-        alert("没钱了。。在首页点一下辣鸡掌门充值。。");
-        location.href = "../main.html";
-    }
-    drawPlayerZmInit();
-
-    drawPlayerMeInit();
-
+function drawCenter() {
     ctx.fillStyle = '#ff6ffb';
     ctx.strokeStyle = '#ff626c';
     ctx.fillRect(387, 360, 250, 50);
-    ctx.font = "20px 楷体";
     ctx.fillStyle = '#EEEEEE';
     ctx.fillText("押注筹码：" + total, 437, 395);
+}
 
+function initEnv() {
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext('2d');
+    ctx.font = "20px 楷体";
+
+    drawPlayerZmInit();
+    drawPlayerMeInit();
+    drawCenter();
     drawMybtn();
-
-    // var flower = getFlowerValueByInt(players[0].cards[0].flower);
-    // var face = getFaceValueByInt(players[0].cards[0].face);
-    // console.log("flower" + flower);
-    // console.log("face" + face);
-
-    localStorage.setItem("zm", localStorage.getItem("zm") - 100);
-    localStorage.setItem("me", localStorage.getItem("me") - 100);
 
     canvas.addEventListener("click", clickCanvas, false);
 
@@ -93,6 +85,10 @@ window.onload = function () {
         this.style.cursor = 'pointer'
     };
 
+}
+function play() {
+    localStorage.setItem("zm", localStorage.getItem("zm") - 100);
+    localStorage.setItem("me", localStorage.getItem("me") - 100);
 }
 
 function redrawBtn() {
@@ -227,7 +223,6 @@ function getFlowerValueByInt(val) {
 }
 
 function getFaceValueByInt(val) {
-
     if (val >= 2 && val <= 10) {
         return val + "";
     } else if (val == 11) {
@@ -252,11 +247,11 @@ CardsType.FLUSH = 3;
 CardsType.STRAIGHT_FLUSH = 4;
 CardsType.BOMB = 5;
 
-function Player(cards) {
+function Player(name, money) {
     var player = {};
-    player.cards = cards;
-    player.name = "unknown";
-    player.money = 0;
+    player.name = name;
+    player.money = money;
+    player.cards = [];
     player.type = CardsType.NORMAL;
     return player;
 }
@@ -269,20 +264,24 @@ function getRandomFace() {
     return Math.floor(Math.random() * 13) + 2;
 }
 
-function createSinglePlayer() {
-    //todo 发牌需要重写
-    var cards = [];
-    for (var i = 0; i < 3; i++) {
-        var flower = getRandomFlower();
-        var face = getRandomFace();
-        var card = new Card(flower, face);
-        cards.push(card);
+function createSinglePlayer(name) {
+    // var cards = [];
+    // for (var i = 0; i < 3; i++) {
+    //     var flower = getRandomFlower();
+    //     var face = getRandomFace();
+    //     var card = new Card(flower, face);
+    //     cards.push(card);
+    // }
+    var money = localStorage.getItem(name);
+    if (money < 100) {
+        alert("没钱了。。在首页点一下辣鸡掌门充值。。");
+        location.href = "../main.html";
     }
-    return new Player(cards);
+    return new Player(name, money);
 }
 
-//生成一副牌
-function initCards() {
+var GameProxy = {};
+GameProxy.initPoker = function () {//生成一副牌
     var cards = [];
     for (var i = 14; i > 1; i--) {
         for (var j = 3; j >= 0; j--) {
@@ -293,23 +292,34 @@ function initCards() {
     return cards;
 }
 
-function createPlayers() {
+GameProxy.shufflePoker = function (cards) {//洗牌
+    for (var i = 0; i < 52; i++) {
+        var rd = Math.floor(Math.random() * 53);//0-52
+        var temp = cards[i];//交换
+        cards[i] = cards[rd];
+        cards[rd] = temp;
+    }
+    return cards;
+}
+
+GameProxy.dealPoker = function (cards, players) {//发牌
+    for (var i = 0; i < players.length; i++) {
+        players[i].cards.push(cards.shift());
+    }
+    return players;
+}
+
+GameProxy.createPlayers = function () {
     var players = [];
-    var zhangmen = createSinglePlayer();
-    var me = createSinglePlayer();
-    zhangmen.name = "掌门";
-    zhangmen.money = localStorage.getItem("zm");
-    me.name = "帅哥";
-    me.money = localStorage.getItem("me");
+    var zhangmen = createSinglePlayer("zm");
+    var me = createSinglePlayer("me");
     players.push(zhangmen);
     players.push(me);
-
-    for (var p in players) {
-        console.log("created player-->" + players[p].name + "余额:" + players[p].money + "--->花色" + players[p].cards[0].flower + "牌面"
-            + players[p].cards[0].face + "，花色" + players[p].cards[1].flower + "牌面" + players[p].cards[1].face
-            + "，花色" + players[p].cards[2].flower + "牌面" + players[p].cards[2].face);
-    }
-
+    // for (var p in players) {
+    //     console.log("created player-->" + players[p].name + "余额:" + players[p].money + "--->花色" + players[p].cards[0].flower + "牌面"
+    //         + players[p].cards[0].face + "，花色" + players[p].cards[1].flower + "牌面" + players[p].cards[1].face
+    //         + "，花色" + players[p].cards[2].flower + "牌面" + players[p].cards[2].face);
+    // }
     return players;
 }
 
@@ -325,12 +335,12 @@ function sortPlayerCardsByFace(player) {
             }
         }
     }
-    var log = "";
-    for (var i = 0; i < 3; i++) {
-        log += player.cards[i].face + " "
-
-    }
-    console.log("排序后：" + log);
+    // var log = "";
+    // for (var i = 0; i < 3; i++) {
+    //     log += player.cards[i].face + " "
+    //
+    // }
+    // console.log("排序后：" + log);
     return player;
 }
 
@@ -341,12 +351,12 @@ function movePair2Front(player) {
         player.cards[0] = player.cards[2];
         player.cards[2] = tempCard;
     }
-    var log = "";
-    for (var i = 0; i < 3; i++) {
-        log += player.cards[i].face + " "
-
-    }
-    console.log("放前面后：" + log);
+    // var log = "";
+    // for (var i = 0; i < 3; i++) {
+    //     log += player.cards[i].face + " "
+    //
+    // }
+    // console.log("放前面后：" + log);
     return player;
 }
 
@@ -358,7 +368,7 @@ function initMoney() {
 }
 
 //豹子
-function isBmob(player) {
+function isBomb(player) {
     return player.cards[0].face == player.cards[1].face
         && player.cards[1].face == player.cards[2].face;
 }
@@ -394,13 +404,13 @@ function setPlayerCardsType(player) {
     if (isFlush(player)) {
         if (isStraight(player)) {// 顺金
             player.type = CardsType.STRAIGHT_FLUSH;
-        } else {// 同花
+        } else {//金花
             player.type = CardsType.FLUSH;
         }
     } else if (isStraight(player)) {// 顺子
         player.type = CardsType.STRAIGHT;
     } else if (isPair(player)) {
-        if (isBmob(player)) {// 豹子
+        if (isBomb(player)) {// 豹子
             player.type = CardsType.BOMB;
         } else {// 对子
             player.type = CardsType.PAIR;
@@ -436,21 +446,16 @@ function showCardsType(type) {
 
 }
 
-/*function sumCardsFace(cards) {
- var sum = 0;
- for (var i in cards) {
- sum += cards[i].face
- }
- return sum;
- }*/
-
 function compare(player1, player2) {
+    //player1比牌方，player2被比牌
+    //排好序
     player1 = sortPlayerCardsByFace(player1);
     player2 = sortPlayerCardsByFace(player2);
+    //计算出牌的类型
     player1 = setPlayerCardsType(player1);
     player2 = setPlayerCardsType(player2);
-    console.log("我是：" + showCardsType(player1.type));
-    console.log("掌门是：" + showCardsType(player2.type));
+    console.log("比牌方是：" + showCardsType(player1.type));
+    console.log("被比牌是：" + showCardsType(player2.type));
     if (player1.type > player2.type) {
         return true;
     } else if (player1.type < player2.type) {
